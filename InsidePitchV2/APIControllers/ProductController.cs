b2bbbe46599094debe1f7605b5530d0f6cc0ba02
Shelby Jones <shelby-j@users.noreply.github.com>
2020-06-17 17:06:06 -0500

    Add project files.

---
+++ InsidePitchV2/APIControllers/ProductController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InsidePitchV2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace InsidePitchV2.APIControllers
+{
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext _DbContext;
+        public ProductController(ApplicationDbContext dbcontext) {
+            _DbContext = dbcontext;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _DbContext.Products.ToList();
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut]
+        public void Put([FromBody] CreateProductViewModel viewModel)
+        {
+            _DbContext.Products.Add(new Product {Name = viewModel.Name, Price = viewModel.Price });
+            _DbContext.SaveChanges();
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+public class CreateProductViewModel { 
+    public string Name { get; set; }
+    public float Price { get; set; }
+}
---
+++ InsidePitchV2/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using InsidePitchV2.Models;
+
+namespace InsidePitchV2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private ApplicationDbContext _DbContext;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbContext)
+        {
+            _logger = logger;
+            _DbContext = dbContext;
+        }
+
+        public IActionResult Test()
+        {
+            _DbContext.Products.Add(new Product() {Name = "TestProduct", Price = 150 });
+            _DbContext.SaveChanges();
+            return View(_DbContext.Products.ToList());
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
