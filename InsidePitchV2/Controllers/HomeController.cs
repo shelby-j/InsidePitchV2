d01d484a2195bd7d7700fb45f4b8998b6136035b
Shelby Jones <shelby-j@users.noreply.github.com>
2020-06-18 14:36:42 -0500

    Added a link to the products page; made a registration page

--- InsidePitchV2/Controllers/HomeController.cs
+++ InsidePitchV2/Controllers/HomeController.cs
@@ -37,6 +37,10 @@ namespace InsidePitchV2.Controllers
         {
             return View();
         }
+        public IActionResult Register()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
