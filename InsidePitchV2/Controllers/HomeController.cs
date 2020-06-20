a3ea145851bfbf108981310f0d05605c5ccc2471
Shelby Jones <shelby-j@users.noreply.github.com>
2020-06-20 16:21:35 -0500

    Added login page

--- InsidePitchV2/Controllers/HomeController.cs
+++ InsidePitchV2/Controllers/HomeController.cs
@@ -42,6 +42,11 @@ namespace InsidePitchV2.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
